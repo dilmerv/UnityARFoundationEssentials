a6ad208e6b97e5f5a36da780411e1fb2cc81834a
Dilmer Valecillos <dilmervst@gmail.com>
2019-06-19 23:35:02 -0600

    Refactoring TrackedImageInfoManager

--- Assets/Scripts/TrackedImageInfoManager.cs
+++ Assets/Scripts/TrackedImageInfoManager.cs
@@ -1,45 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.XR.ARSubsystems;
+﻿using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
-/// This component listens for images detected by the <c>XRImageTrackingSubsystem</c>
-/// and overlays some information as well as the source Texture2D on top of the
-/// detected image.
-/// </summary>
 [RequireComponent(typeof(ARTrackedImageManager))]
 public class TrackedImageInfoManager : MonoBehaviour
 {
-    [SerializeField]
-    [Tooltip("The camera to set on the world space UI canvas for each instantiated image info.")]
-    Camera m_WorldSpaceCanvasCamera;
-
-    /// <summary>
-    /// The prefab has a world space UI canvas,
-    /// which requires a camera to function properly.
-    /// </summary>
-    public Camera worldSpaceCanvasCamera
-    {
-        get { return m_WorldSpaceCanvasCamera; }
-        set { m_WorldSpaceCanvasCamera = value; }
-    }
-
-    [SerializeField]
-    [Tooltip("If an image is detected but no source texture can be found, this texture is used instead.")]
-    Texture2D m_DefaultTexture;
-
-    /// <summary>
-    /// If an image is detected but no source texture can be found,
-    /// this texture is used instead.
-    /// </summary>
-    public Texture2D defaultTexture
-    {
-        get { return m_DefaultTexture; }
-        set { m_DefaultTexture = value; }
-    }
-
     ARTrackedImageManager m_TrackedImageManager;
 
     void Awake()
