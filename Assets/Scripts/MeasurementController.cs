410aa565fb8bde88b15a031422dc849600432bd6
Dilmer Valecillos <dilmervst@gmail.com>
2019-06-23 04:33:23 -0600

    Added Measuring Tape and Placing of TVs examples

---
+++ Assets/Scripts/MeasurementController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using TMPro;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class MeasurementController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject measurementPointPrefab;
+
+    [SerializeField]
+    private float measurementFactor = 39.37f;
+
+    [SerializeField]
+    private Vector3 offsetMeasurement = Vector3.zero;
+
+    [SerializeField]
+    private GameObject welcomePanel;
+
+    [SerializeField]
+    private Button dismissButton;
+
+    [SerializeField]
+    private TextMeshPro distanceText;
+
+    [SerializeField]
+    private ARCameraManager arCameraManager;
+    
+    private LineRenderer measureLine;
+
+    private ARRaycastManager arRaycastManager;
+
+    private GameObject startPoint;
+
+    private GameObject endPoint;
+
+    private Vector2 touchPosition = default;
+
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Awake() 
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+        
+        startPoint = Instantiate(measurementPointPrefab, Vector3.zero, Quaternion.identity);
+        endPoint = Instantiate(measurementPointPrefab, Vector3.zero, Quaternion.identity);
+   
+        measureLine = GetComponent<LineRenderer>();
+        
+        startPoint.SetActive(false);
+        endPoint.SetActive(false);
+
+        dismissButton.onClick.AddListener(Dismiss);
+    }
+
+    private void Dismiss() => welcomePanel.SetActive(false);
+
+    private void OnEnable() 
+    {
+        if(measurementPointPrefab == null)
+        {
+            Debug.LogError("measurementPointPrefab must be set");
+            enabled = false;
+        }    
+    }
+    
+    void Update()
+    { 
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                touchPosition = touch.position;
+
+                if(arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+                {
+                    startPoint.SetActive(true);
+
+                    Pose hitPose = hits[0].pose;
+                    startPoint.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
+                }
+            }
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                touchPosition = touch.position;
+                
+                if(arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+                {
+                    measureLine.gameObject.SetActive(true);
+                    endPoint.SetActive(true);
+
+                    Pose hitPose = hits[0].pose;
+                    endPoint.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
+                }
+            }
+        }
+
+        if(startPoint.activeSelf && endPoint.activeSelf)
+        {
+            distanceText.transform.position = endPoint.transform.position + offsetMeasurement;
+            distanceText.transform.rotation = endPoint.transform.rotation;
+            measureLine.SetPosition(0, startPoint.transform.position);
+            measureLine.SetPosition(1, endPoint.transform.position);
+
+            distanceText.text = $"Distance: {(Vector3.Distance(startPoint.transform.position, endPoint.transform.position) * measurementFactor).ToString("F2")} in";
+        }
+    }
+}
---
+++ Assets/Scripts/PlacementWithManyController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlacementWithManyController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] placedPrefabs;
+
+    [SerializeField]
+    private GameObject welcomePanel;
+
+    [SerializeField]
+    private int maxNumberOfTVs = 1;
+
+    [SerializeField]
+    private Button dismissButton;
+
+    private List<GameObject> addedInstances = new List<GameObject>();
+
+    private Vector2 touchPosition = default;
+
+    public GameObject[] PlacedPrefab
+    {
+        get 
+        {
+            return placedPrefabs;
+        }
+        set 
+        {
+            placedPrefabs = value;
+        }
+    }
+
+    [SerializeField]
+    private VideoClip[] videoClips;
+
+    private ARRaycastManager arRaycastManager;
+
+    void Awake() 
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+        dismissButton.onClick.AddListener(Dismiss);
+    }
+    private void Dismiss() => welcomePanel.SetActive(false);
+
+    void Update()
+    {
+        // on double touch swap clips
+        if(Input.touchCount >= 2)
+        {
+            foreach(GameObject go in placedPrefabs)
+            {
+                go.GetComponent<VideoPlayer>().clip = videoClips[Random.Range(0, videoClips.Length - 1)];
+            }
+        }
+
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                touchPosition = touch.position;
+
+                if(arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+                {
+                    var hitPose = hits[0].pose;
+                    if(addedInstances.Count < maxNumberOfTVs)
+                    {
+                        GameObject randomPrefab =  placedPrefabs[Random.Range(0, placedPrefabs.Length - 1)];
+                        GameObject addedPrefab = Instantiate(randomPrefab, hitPose.position, hitPose.rotation);
+                        addedInstances.Add(addedPrefab);
+                    }
+                }
+            }
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                touchPosition = touch.position;
+                
+                if(arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = hits[0].pose;
+                    if(addedInstances.Count > 0)
+                    {
+                       GameObject lastAdded = addedInstances[addedInstances.Count - 1];
+                       lastAdded.transform.position = hitPose.position;
+                       lastAdded.transform.rotation = hitPose.rotation;
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+}
