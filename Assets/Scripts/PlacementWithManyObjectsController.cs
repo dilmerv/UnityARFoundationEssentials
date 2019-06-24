263829275b8e08ab83ae692941748b0115373cc2
Dilmer Valecillos <dilmervst@gmail.com>
2019-06-24 07:32:52 -0600

    Added cloud points example

---
+++ Assets/Scripts/PlacementWithManyObjectsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlacementWithManyObjectsController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject placedPrefab;
+
+    [SerializeField]
+    private GameObject welcomePanel;
+
+    [SerializeField]
+    private Button dismissButton;
+
+    private Vector2 touchPosition = default;
+
+    private ARRaycastManager arRaycastManager;
+
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Awake() 
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+        dismissButton.onClick.AddListener(Dismiss);
+    }
+    private void Dismiss() => welcomePanel.SetActive(false);
+
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                touchPosition = touch.position;
+
+                if(arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+                {
+                    var hitPose = hits[0].pose;
+                    Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+                }
+            }
+        }
+
+    }
+}
