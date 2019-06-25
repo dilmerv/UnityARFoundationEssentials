07e8281ff8eddde2b27ef165e2c81c0d6b734872
Dilmer Valecillos <dilmervst@gmail.com>
2019-06-25 00:17:37 -0600

    Added Raycasting example for object dragging

---
+++ Assets/Scripts/PlacementWithDraggingDroppingController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlacementWithDraggingDroppingController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject placedPrefab;
+
+    [SerializeField]
+    private GameObject welcomePanel;
+
+    [SerializeField]
+    private Button dismissButton;
+
+    [SerializeField]
+    private Camera arCamera;
+
+    private GameObject placedObject;
+
+    private Vector2 touchPosition = default;
+
+    private ARRaycastManager arRaycastManager;
+
+    private bool onTouchHold = false;
+
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Awake() 
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+        dismissButton.onClick.AddListener(Dismiss);
+    }
+    private void Dismiss() => welcomePanel.SetActive(false);
+
+    void Update()
+    {
+        // do not capture events unless the welcome panel is hidden
+        if(welcomePanel.activeSelf)
+            return;
+
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            
+            touchPosition = touch.position;
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                Ray ray = arCamera.ScreenPointToRay(touch.position);
+                RaycastHit hitObject;
+                if(Physics.Raycast(ray, out hitObject))
+                {
+                    if(hitObject.transform.name.Contains("PlacedObject"))
+                    {
+                        onTouchHold = true;
+                    }
+                }
+            }
+
+            if(touch.phase == TouchPhase.Ended)
+            {
+                onTouchHold = false;
+            }
+        }
+
+        if(arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+        {
+            Pose hitPose = hits[0].pose;
+
+            if(placedObject == null)
+            {
+                placedObject = Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+            }
+            else 
+            {
+                if(onTouchHold)
+                {
+                    placedObject.transform.position = hitPose.position;
+                    placedObject.transform.rotation = hitPose.rotation;
+                }
+            }
+        }
+    }
+}
