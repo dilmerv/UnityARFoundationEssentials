1193c4b7f479d6bb913f19c0447abdb25d2621b4
Dilmer Valecillos <dilmervalecillos@Dilmers-MacBook-Pro.local>
2019-07-08 21:34:13 -0600

    Added new example to show how to select objects in a scene

---
+++ Assets/Scripts/PlacementWithMultipleDraggingDroppingController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlacementWithMultipleDraggingDroppingController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject placedPrefab;
+
+    [SerializeField]
+    private GameObject welcomePanel;
+
+    [SerializeField]
+    private Button dismissButton;
+
+    [SerializeField]
+    private Camera arCamera;
+
+    private PlacementObject[] placedObjects;
+
+    private Vector2 touchPosition = default;
+
+    private ARRaycastManager arRaycastManager;
+
+    private bool onTouchHold = false;
+
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private PlacementObject lastSelectedObject;
+    void Awake() 
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+        dismissButton.onClick.AddListener(Dismiss);
+    }
+    private void Dismiss() => welcomePanel.SetActive(false);
+
+    void Update()
+    {
+        // do not capture events unless the welcome panel is hidden
+        if(welcomePanel.activeSelf)
+            return;
+
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            
+            touchPosition = touch.position;
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                Ray ray = arCamera.ScreenPointToRay(touch.position);
+                RaycastHit hitObject;
+                if(Physics.Raycast(ray, out hitObject))
+                {
+                    lastSelectedObject = hitObject.transform.GetComponent<PlacementObject>();
+                    if(lastSelectedObject != null)
+                    {
+                        PlacementObject[] allOtherObjects = FindObjectsOfType<PlacementObject>();
+                        foreach(PlacementObject placementObject in allOtherObjects)
+                        {
+                            placementObject.Selected = placementObject == lastSelectedObject;
+                        }
+                    }
+                }
+            }
+
+            if(touch.phase == TouchPhase.Ended)
+            {
+                lastSelectedObject.Selected = false;
+            }
+        }
+
+        if(arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+        {
+            Pose hitPose = hits[0].pose;
+
+            if(lastSelectedObject == null)
+            {
+                lastSelectedObject = Instantiate(placedPrefab, hitPose.position, hitPose.rotation).GetComponent<PlacementObject>();
+            }
+            else 
+            {
+                if(lastSelectedObject.Selected)
+                {
+                    lastSelectedObject.transform.position = hitPose.position;
+                    lastSelectedObject.transform.rotation = hitPose.rotation;
+                }
+            }
+        }
+    }
+}
