66d46b4ad124e5b4e0f229088817980810037b94
Dilmer Valecillos <dilmervst@gmail.com>
2019-07-22 15:36:28 -0600

    Added placement rotation script and demo scene

---
+++ Assets/Scripts/PlacementRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlacementObject))]
+public class PlacementRotation : MonoBehaviour
+{
+    private PlacementObject placementObject;
+
+    [SerializeField]
+    private Vector3 rotationSpeed = Vector3.zero;
+
+    void Awake()
+    {
+        placementObject = GetComponent<PlacementObject>();
+    }
+
+    void Update()
+    {
+        if(placementObject.Selected)
+        {
+            transform.Rotate(rotationSpeed * Time.deltaTime, Space.World);
+        }   
+    }
+}
