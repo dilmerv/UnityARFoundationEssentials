14ab922dce083310590c2ea20bf0108a3c44562e
Dilmer Valecillos <dilmervst@gmail.com>
2019-10-07 15:42:45 -0600

    Added a image tracking ruttime scene example

--- Assets/Scripts/TrackedImageInfoRuntimeManager.cs
+++ Assets/Scripts/TrackedImageInfoRuntimeManager.cs
@@ -1,97 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-using UnityEngine.Jobs;
-using Unity.Collections;
-using System.Collections;
 
-[RequireComponent(typeof(ARTrackedImageManager))]
 public class TrackedImageInfoRuntimeManager : MonoBehaviour
 {
     [SerializeField]
     private Text debugLog;
 
     [SerializeField]
-    private Text jobState;
+    private Text currentImageText;
+
+    [SerializeField]
+    private GameObject prefabOnTrack;
 
     [SerializeField]
     private Vector3 scaleFactor = new Vector3(0.1f,0.1f,0.1f);
-    
+
     [SerializeField]
+    XRReferenceImageLibrary xrReferenceImageLibrary;
+    
     private ARTrackedImageManager trackImageManager;
 
-    private MutableRuntimeReferenceImageLibrary runtimeReferenceImageLibrary;
-
-    [SerializeField]
-    private Texture2D dynamicTexture;
-
-    [SerializeField]
-    private XRReferenceImage xRReferenceImage;
-
-    private NativeSlice<byte> imageBytes;
-    
-
-    void Awake()
+    void Start()
     {
         debugLog.text += "Creating Runtime Mutable Image Library\n";
-        runtimeReferenceImageLibrary = trackImageManager.CreateRuntimeLibrary() as MutableRuntimeReferenceImageLibrary;
-        trackImageManager.referenceLibrary = runtimeReferenceImageLibrary;
-        debugLog.text += "Created Runtime Mutable Image Library\n";
 
-        jobState.text = "Starting...";
+        trackImageManager = gameObject.AddComponent<ARTrackedImageManager>();
+        trackImageManager.referenceLibrary = trackImageManager.CreateRuntimeLibrary(xrReferenceImageLibrary);
+        trackImageManager.maxNumberOfMovingImages = 3;
+        trackImageManager.trackedImagePrefab = prefabOnTrack;
 
-        StartCoroutine(AddImageJob());
-    }
-
-    public IEnumerator AddImageJob()
-    {
-        yield return null;
+        trackImageManager.enabled = true;
         
-        debugLog.text += "Add Image Job\n";
-
-        var firstGuid = new SerializableGuid(0,0);
-        var secondGuid = new SerializableGuid(0,0);
-
-        //XRReferenceImage newImage = new XRReferenceImage(firstGuid, secondGuid, new Vector2(0.1f,0.1f), Guid.NewGuid().ToString(), dynamicTexture);
-        
-        try
-        {
-            Debug.Log(xRReferenceImage.ToString());
-
-            NativeArray<byte> imageBytes = new NativeArray<byte>(dynamicTexture.GetRawTextureData(), Allocator.Persistent);
-            
-            debugLog.text += $"TextureFormat.RGBA32 supported: {runtimeReferenceImageLibrary.IsTextureFormatSupported(TextureFormat.RGBA32)}\n";
-
-            var jobHandle = runtimeReferenceImageLibrary.ScheduleAddImageJob(imageBytes.Slice(), new Vector2Int(dynamicTexture.width, dynamicTexture.height), TextureFormat.RGBA32, xRReferenceImage);
-
-            while(!jobHandle.IsCompleted)
-            {
-                jobState.text = "Running...";
-            }
-
-            jobState.text = "Completed...";
-            debugLog.text += $"Job Completed ({runtimeReferenceImageLibrary.count})\n";
-            debugLog.text += $"Supported Texture Count ({runtimeReferenceImageLibrary.supportedTextureFormatCount})\n";
-            debugLog.text += $"trackImageManager.trackables.count ({trackImageManager.trackables.count})\n";
-            debugLog.text += $"trackImageManager.trackedImagePrefab.name ({trackImageManager.trackedImagePrefab.name})\n";
-            debugLog.text += $"trackImageManager.maxNumberOfMovingImages ({trackImageManager.maxNumberOfMovingImages})\n";
-            debugLog.text += $"trackImageManager.supportsMutableLibrary ({trackImageManager.subsystem.SubsystemDescriptor.supportsMutableLibrary})\n";
-            debugLog.text += $"trackImageManager.requiresPhysicalImageDimensions ({trackImageManager.subsystem.SubsystemDescriptor.requiresPhysicalImageDimensions})\n";
-        }
-        catch(Exception e)
-        {
-            debugLog.text = e.ToString();
-        }
-    }
-
-    void OnEnable()
-    {
         trackImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+        
+        ShowTrackerInfo();
     }
 
+    public void ShowTrackerInfo()
+    {
+        var runtimeReferenceImageLibrary = trackImageManager.referenceLibrary as MutableRuntimeReferenceImageLibrary;
+
+        debugLog.text += $"TextureFormat.RGBA32 supported: {runtimeReferenceImageLibrary.IsTextureFormatSupported(TextureFormat.RGBA32)}\n";
+        debugLog.text += $"Supported Texture Count ({runtimeReferenceImageLibrary.supportedTextureFormatCount})\n";
+        debugLog.text += $"trackImageManager.trackables.count ({trackImageManager.trackables.count})\n";
+        debugLog.text += $"trackImageManager.trackedImagePrefab.name ({trackImageManager.trackedImagePrefab.name})\n";
+        debugLog.text += $"trackImageManager.maxNumberOfMovingImages ({trackImageManager.maxNumberOfMovingImages})\n";
+        debugLog.text += $"trackImageManager.supportsMutableLibrary ({trackImageManager.subsystem.SubsystemDescriptor.supportsMutableLibrary})\n";
+        debugLog.text += $"trackImageManager.requiresPhysicalImageDimensions ({trackImageManager.subsystem.SubsystemDescriptor.requiresPhysicalImageDimensions})\n";
+    }
     void OnDisable()
     {
         trackImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
@@ -99,23 +57,22 @@ public class TrackedImageInfoRuntimeManager : MonoBehaviour
 
     void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
-        debugLog.text = "OnTrackedImagesChanged";
-
         foreach (ARTrackedImage trackedImage in eventArgs.added)
         {
-            debugLog.text = "Tracked Image Added: " + trackedImage.name;
-            UpdateARImage(trackedImage);
+            // Display the name of the tracked image in the canvas
+            currentImageText.text = trackedImage.referenceImage.name;
+            // Give the initial image a reasonable default scale
+            trackedImage.transform.localScale = 
+                new Vector3(-trackedImage.referenceImage.size.x, 0.005f, -trackedImage.referenceImage.size.y);
         }
 
         foreach (ARTrackedImage trackedImage in eventArgs.updated)
         {
-            debugLog.text = "Tracked Image Updated: " + trackedImage.name;
-            UpdateARImage(trackedImage);
+            // Display the name of the tracked image in the canvas
+            currentImageText.text = trackedImage.referenceImage.name;
+            // Give the initial image a reasonable default scale
+            trackedImage.transform.localScale = 
+                new Vector3(-trackedImage.referenceImage.size.x, 0.005f, -trackedImage.referenceImage.size.y);
         }
-    }
-
-    private void UpdateARImage(ARTrackedImage trackedImage)
-    {
-
     }
 }
