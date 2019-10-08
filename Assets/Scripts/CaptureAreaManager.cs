c1df43d1b207f7c676767f357faeea1e6202edfd
Dilmer Valecillos <dilmervst@gmail.com>
2019-10-08 07:10:52 -0600

    Added new texture load runtime example for image tracking

---
+++ Assets/Scripts/CaptureAreaManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CaptureAreaManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button topLeft;
+
+    [SerializeField]
+    private Button bottomRight;
+
+    [SerializeField]
+    private Rect captureRegion;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var topLeftRect = topLeft.GetComponent<Rect>();
+
+        captureRegion.position = new Vector2(topLeftRect.x, topLeftRect.y);
+    }
+}
---
+++ Assets/Scripts/TrackedImageInfoRuntimeSaveManager.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class TrackedImageInfoRuntimeSaveManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text debugLog;
+
+    [SerializeField]
+    private Text jobLog;
+
+    [SerializeField]
+    private Text currentImageText;
+
+    [SerializeField]
+    private Button firstTextureButton;
+
+    [SerializeField]
+    private Button secondTextureButton;
+
+    [SerializeField]
+    private GameObject prefabOnTrack;
+
+    [SerializeField]
+    private Vector3 scaleFactor = new Vector3(0.1f,0.1f,0.1f);
+
+    [SerializeField]
+    private XRReferenceImageLibrary runtimeImageLibrary;
+    
+    private ARTrackedImageManager trackImageManager;
+
+    [SerializeField]
+    private Texture2D firstTexture;
+
+    [SerializeField]
+    private Texture2D secondTexture;
+
+    void Start()
+    {
+        debugLog.text += "Creating Runtime Mutable Image Library\n";
+
+        trackImageManager = gameObject.AddComponent<ARTrackedImageManager>();
+        trackImageManager.referenceLibrary = trackImageManager.CreateRuntimeLibrary(runtimeImageLibrary);
+        trackImageManager.maxNumberOfMovingImages = 3;
+        trackImageManager.trackedImagePrefab = prefabOnTrack;
+
+        trackImageManager.enabled = true;
+
+        trackImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+        
+        ShowTrackerInfo();
+
+        firstTextureButton.onClick.AddListener(() => StartCoroutine(AddImageJob(firstTexture)));
+
+        secondTextureButton.onClick.AddListener(() => StartCoroutine(AddImageJob(secondTexture)));
+
+    }
+    public void ShowTrackerInfo()
+    {
+        var runtimeReferenceImageLibrary = trackImageManager.referenceLibrary as MutableRuntimeReferenceImageLibrary;
+
+        debugLog.text += $"TextureFormat.RGBA32 supported: {runtimeReferenceImageLibrary.IsTextureFormatSupported(TextureFormat.RGBA32)}\n";
+        debugLog.text += $"Supported Texture Count ({runtimeReferenceImageLibrary.supportedTextureFormatCount})\n";
+        debugLog.text += $"trackImageManager.trackables.count ({trackImageManager.trackables.count})\n";
+        debugLog.text += $"trackImageManager.trackedImagePrefab.name ({trackImageManager.trackedImagePrefab.name})\n";
+        debugLog.text += $"trackImageManager.maxNumberOfMovingImages ({trackImageManager.maxNumberOfMovingImages})\n";
+        debugLog.text += $"trackImageManager.supportsMutableLibrary ({trackImageManager.subsystem.SubsystemDescriptor.supportsMutableLibrary})\n";
+        debugLog.text += $"trackImageManager.requiresPhysicalImageDimensions ({trackImageManager.subsystem.SubsystemDescriptor.requiresPhysicalImageDimensions})\n";
+    }
+    void OnDisable()
+    {
+        trackImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    public IEnumerator AddImageJob(Texture2D texture2D)
+    {
+        yield return null;
+        
+        debugLog.text = string.Empty;
+    
+        debugLog.text += "Adding image\n";
+
+        jobLog.text = "Job Starting...";
+
+        var firstGuid = new SerializableGuid(0,0);
+        var secondGuid = new SerializableGuid(0,0);
+        
+        XRReferenceImage newImage = new XRReferenceImage(firstGuid, secondGuid, new Vector2(0.1f,0.1f), Guid.NewGuid().ToString(), texture2D);
+        
+        try
+        {
+            Debug.Log(newImage.ToString());
+
+            MutableRuntimeReferenceImageLibrary mutableRuntimeReferenceImageLibrary = trackImageManager.referenceLibrary as MutableRuntimeReferenceImageLibrary;
+            
+            debugLog.text += $"TextureFormat.RGBA32 supported: {mutableRuntimeReferenceImageLibrary.IsTextureFormatSupported(TextureFormat.RGBA32)}\n";
+
+            debugLog.text += $"TextureFormat size: {texture2D.width}px width {texture2D.height}px height\n";
+
+            var jobHandle = mutableRuntimeReferenceImageLibrary.ScheduleAddImageJob(texture2D, Guid.NewGuid().ToString(), 0.1f);
+
+            while(!jobHandle.IsCompleted)
+            {
+                jobLog.text = "Job Running...";
+            }
+
+            jobLog.text = "Job Completed...";
+            debugLog.text += $"Job Completed ({mutableRuntimeReferenceImageLibrary.count})\n";
+            debugLog.text += $"Supported Texture Count ({mutableRuntimeReferenceImageLibrary.supportedTextureFormatCount})\n";
+            debugLog.text += $"trackImageManager.trackables.count ({trackImageManager.trackables.count})\n";
+            debugLog.text += $"trackImageManager.trackedImagePrefab.name ({trackImageManager.trackedImagePrefab.name})\n";
+            debugLog.text += $"trackImageManager.maxNumberOfMovingImages ({trackImageManager.maxNumberOfMovingImages})\n";
+            debugLog.text += $"trackImageManager.supportsMutableLibrary ({trackImageManager.subsystem.SubsystemDescriptor.supportsMutableLibrary})\n";
+            debugLog.text += $"trackImageManager.requiresPhysicalImageDimensions ({trackImageManager.subsystem.SubsystemDescriptor.requiresPhysicalImageDimensions})\n";
+        }
+        catch(Exception e)
+        {
+            debugLog.text = e.ToString();
+        }
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (ARTrackedImage trackedImage in eventArgs.added)
+        {
+            // Display the name of the tracked image in the canvas
+            currentImageText.text = trackedImage.referenceImage.name;
+            trackedImage.transform.Rotate(Vector3.up, 180);
+        }
+
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
+        {
+            // Display the name of the tracked image in the canvas
+            currentImageText.text = trackedImage.referenceImage.name;
+            trackedImage.transform.Rotate(Vector3.up, 180);
+        }
+    }
+}
